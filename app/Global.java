a667371330de23c390166347645df027841cebcc
Diogo Nunes <dialex.nunes@gmail.com>
2013-08-19 18:30:17 +0100

    first attempt to solve problem http://stackoverflow.com/q/18298173/675577

--- app/Global.java
+++ app/Global.java
@@ -1,17 +1,32 @@
 import play.*;
 import play.libs.*;
+
 import com.avaje.ebean.Ebean;
+
 import models.*;
+
 import java.util.*;
 
 /**
  * Checks is database is empty, and if so populates with some test data.
  */
 public class Global extends GlobalSettings {
-    @Override
+    
+	@Override
     public void onStart(Application app) {
-        if (User.find.findRowCount() == 0) {				// if DB is empty
-            Ebean.save((List) Yaml.load("test-data.yml"));	// populate it
+    	// load data to populate the database
+		@SuppressWarnings("unchecked")
+		Map<String,List<Object>> populateData = (Map<String,List<Object>>) Yaml.load("test-data.yml");
+    	
+    	// save all entities by type
+        Ebean.save(populateData.get("users"));
+        Ebean.save(populateData.get("proposals"));
+        Ebean.save(populateData.get("tags"));
+    	
+    	// save all relationships between entities
+        for(Object tag: populateData.get("tags")) {
+            Ebean.saveManyToManyAssociations(tag, "proposals");
         }
     }
+    
 }
