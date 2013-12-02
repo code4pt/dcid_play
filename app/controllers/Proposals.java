e4a1eaa063867cf2f5be4bc7fb0938e0fb9e2aca
Diogo Nunes <dialex.nunes@gmail.com>
2013-12-02 23:14:39 +0000

    prepare to follow http://www.playframework.com/documentation/2.2.1/JavaGuide5

--- app/controllers/Proposals.java
+++ app/controllers/Proposals.java
@@ -1,10 +1,22 @@
 package controllers;
 
+import java.util.*;
+
+import static play.data.Form.form;
 import play.mvc.*;
+import models.*;
+import views.html.snippets.*;
 
 @Security.Authenticated(Secured.class)
 public class Proposals extends Controller {
 	
+	public static Result createProposal() {
+	    Proposal newProposal = Proposal.createAndSave("New proposal",
+	    		"", "", "",
+	    		request().username());
+	    
+	    return ok(displayProposalDetail.render(newProposal));
+	}
 //	public static Result propose() {
 //	    Proposal newProposal = Proposal.createAndSave(title, problem, solution, benefits,
 //	    		request().username())
